600254d50922489b67102dabba925fc4f5faedd2
bvcrisostomo2 <bvcrisostomo2@gmail.com>
2019-04-06 01:09:01 +0800

    Updates

---
+++ precache-manifest.61bace215ea3abeae14d713928c4a16c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa1ce8e3018ba92340bb",
+    "url": "/react-portfolio/static/css/main.24ff7e48.chunk.css"
+  },
+  {
+    "revision": "fa1ce8e3018ba92340bb",
+    "url": "/react-portfolio/static/js/main.2602a662.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "a2c5bc0ae296fa74bb03",
+    "url": "/react-portfolio/static/css/2.27faf668.chunk.css"
+  },
+  {
+    "revision": "a2c5bc0ae296fa74bb03",
+    "url": "/react-portfolio/static/js/2.076741d6.chunk.js"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/react-portfolio/static/media/man.bd80794f.png"
+  },
+  {
+    "revision": "59a5f4dc14019e9178bdd58ee91680d0",
+    "url": "/react-portfolio/index.html"
+  }
+];
